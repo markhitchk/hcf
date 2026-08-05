7b68d54b00ecc79c1a26f92f3357d2a659484168
markhitchk <mark.hitchk@gmail.com>
2026-08-04 20:41:43 -0700

    Delete holidays directory

--- holidays/holidays.js
+++
@@ -1,8 +0,0 @@
-/* Compatibility loader. Canonical file: addons/celebrations/holidays.js */
-(function () {
-  "use strict";
-  var script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/gh/markhitchk/hcf@main/addons/celebrations/holidays.js";
-  script.defer = true;
-  document.head.appendChild(script);
-})();
